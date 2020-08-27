227568f528b5293c20c676edb4cb343cfb908960
juan <juan@stateside.agency>
2020-08-26 18:30:54 -0600

    cleanup

--- wp-content/themes/Avilon copy/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function customtheme_add_woocommerce_support() {
-        add_theme_support( 'woocommerce' );
-    }
-    
-    add_action( 'after_setup_theme', 'customtheme_add_woocommerce_support' );
-?>
